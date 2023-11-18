5bfc261f387f3be3288c2bb756fa2c4da528ee76
David Antoni Torres Carcache <110353857+DarcoProgramador@users.noreply.github.com>
2023-11-18 05:47:34 +0000

    update final solution
    Add a final solution to the app.py file

--- app.py
+++ app.py
@@ -1,21 +1,28 @@
 import random
 
-opciones = ('rock', 'paper', 'scissors')
+opciones = ('piedra', 'papel', 'tijeras')
+victorias = 0
+derrotas = 0
+empates = 0
 
 while True:
-    eleccion_usuario = input("Elige una opción: rock, paper, scissors: ").lower()
+    eleccion_usuario = input("Elige una opción: piedra, papel, tijeras: ").lower()
     if eleccion_usuario not in opciones:
         print("Opción no válida. Inténtalo de nuevo.")
         continue
     eleccion_oponente = random.choice(opciones)
     if eleccion_usuario == eleccion_oponente:
         print("Es un empate.")
-    elif (eleccion_usuario == "rock" and eleccion_oponente == "scissors") or \
-         (eleccion_usuario == "scissors" and eleccion_oponente == "paper") or \
-         (eleccion_usuario == "paper" and eleccion_oponente == "rock"):
+        empates += 1
+    elif (eleccion_usuario == "piedra" and eleccion_oponente == "tijeras") or \
+         (eleccion_usuario == "tijeras" and eleccion_oponente == "papel") or \
+         (eleccion_usuario == "papel" and eleccion_oponente == "piedra"):
         print("Has ganado.")
+        victorias += 1
     else:
         print("Has perdido.")
+        derrotas += 1
+    print(f"Victorias: {victorias}, Derrotas: {derrotas}, Empates: {empates}")
     jugar_de_nuevo = input("¿Quieres jugar de nuevo? (s/n): ").lower()
     if jugar_de_nuevo != "s":
         break
