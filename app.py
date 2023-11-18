0a754262d5a386762289c3c7510de5862953cb9f
David Antoni Torres Carcache <110353857+DarcoProgramador@users.noreply.github.com>
2023-11-18 05:44:47 +0000

    update app.py
    add my first solution

--- app.py
+++ app.py
@@ -1,2 +1,21 @@
-# write 'hello world' to the console
-print("hello world")
+import random
+
+opciones = ('rock', 'paper', 'scissors')
+
+while True:
+    eleccion_usuario = input("Elige una opción: rock, paper, scissors: ").lower()
+    if eleccion_usuario not in opciones:
+        print("Opción no válida. Inténtalo de nuevo.")
+        continue
+    eleccion_oponente = random.choice(opciones)
+    if eleccion_usuario == eleccion_oponente:
+        print("Es un empate.")
+    elif (eleccion_usuario == "rock" and eleccion_oponente == "scissors") or \
+         (eleccion_usuario == "scissors" and eleccion_oponente == "paper") or \
+         (eleccion_usuario == "paper" and eleccion_oponente == "rock"):
+        print("Has ganado.")
+    else:
+        print("Has perdido.")
+    jugar_de_nuevo = input("¿Quieres jugar de nuevo? (s/n): ").lower()
+    if jugar_de_nuevo != "s":
+        break
